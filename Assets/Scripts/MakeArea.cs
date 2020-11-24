4391c65d85abd2ed17bf65fce0a13fb5b3f85010
ry0 <tan0ry0shiny@gmail.com>
2020-11-24 22:12:00 +0900

    gen_block
    1st_commit

---
+++ Assets/Scripts/MakeArea.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeArea : MonoBehaviour
+{
+    public int max_z; //フィールドの縦幅。5以上の奇数にすること。
+    public int max_x; //フィールドの横幅。5以上の奇数にすること。
+    int z; //フィールド配列の縦の要素番号
+    int x; //フィールド配列の横の要素番号
+    int r; //乱数の値
+    Object wall; //壁オブジェクト
+    GameObject wallgo; //壁のゲームオブジェクト
+    public int[,] field;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int[,] field = new int[max_z, max_x]; //フィールド（0が通路で、1が壁。）
+        wall = Resources.Load("Wall"); //壁オブジェクトを読み込む。
+
+        //上下の外壁（1）の生成
+        for (x = 0; x < max_x; x++) //フィールドの横幅の分だけループする。
+        {
+            for (z = 0; z < max_z; z++) //フィールドの縦幅の分だけループする。
+            {
+                field[z, x] = 1;
+            }
+        }
+
+        //壁の配置
+        for (z = 0; z < max_z; z = z + 1) //フィールドの縦幅の分だけループする。
+        {
+            for (x = 0; x < max_x; x = x + 1) //フィールドの横幅の分だけループする。
+            {
+                if (field[z, x] == 0) //通路なら
+                {
+                    //何も配置しない。
+                }
+                else if (field[z, x] == 1) //壁なら
+                {
+                    Instantiate(wall, new Vector3(5.0f * x, 5.0f, 5.0f * z), Quaternion.identity); //壁を配置する。
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PushBox.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushBox : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Object wall;
+    int z; //フィールド配列の縦の要素番号
+    int x; //フィールド配列の横の要素番号
+    float seconds;
+
+    void Start()
+    {//スタートしたらブロックが一つ落ちる
+        Instantiate(wall, new Vector3(5.0f * x, 5.0f, 5.0f * z), Quaternion.identity);
+        InvokeRepeating("AutoGen", 0f, 1f);
+        //Invoke("Hoge", 5);
+        seconds += Time.deltaTime;
+        if (seconds >= 5)
+        {
+            seconds = 0;
+            Debug.Log("5秒後に実行された");
+        }
+        GameObject[] answers = GameObject.FindGameObjectsWithTag("answer");
+        foreach (GameObject answer in answers) 
+        {
+            // 消す！
+            Destroy(answer);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {//スペースを押したら5X5の範囲に物を並べる。Y座標は3f。
+
+        /*
+        if (Input.GetButtonUp("Jump"))
+        {
+
+            //Instantiate(wall, new Vector3(Random.Range(-5f, 5f), 50.0f, Random.Range(-5f, 5f)), Quaternion.identity); //壁を配置する。
+        }
+        //画面をクリックしたらブロックを削除
+        if(Input.GetMouseButton(0))
+        {
+            */
+            /*
+            Debug.Log("reset");
+            GameObject dewall = GameObject.Find("Wall(Clone)");
+            Destroy(dewall);*//*
+        }
+        */
+
+    }
+
+    void AutoGen()
+    {
+        Instantiate(this.wall, new Vector3(Random.Range(-15f, 15f), transform.position.y, Random.Range(-15f, 15f)), transform.rotation);
+    }
+
+    /*
+    void Hoge()
+    {
+        Debug.Log("3秒後に実行された");
+        GameObject[] answers = GameObject.FindGameObjectsWithTag("answer");
+        foreach (GameObject answer in answers) 
+        {
+            // 消す！
+            Destroy(answer);
+        }
+    }*/
+}
